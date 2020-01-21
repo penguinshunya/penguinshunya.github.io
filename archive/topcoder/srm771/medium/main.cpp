7783196ad97774764ed037b9fc8a18c13a34345c
penguinshunya <penguinshunya@hotmail.com>
2020-01-21 18:49:44 +0900

    modify

--- archive/topcoder/srm771/medium/main.cpp
+++ archive/topcoder/srm771/medium/main.cpp
@@ -2,15 +2,43 @@
 
 #define rep(i, n) for (int i = 0; i < int(n); i++)
 #define rrep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define reps(i, n) for (int i = 1; i <= int(n); i++)
+#define rreps(i, n) for (int i = int(n); i >= 1; i--)
 #define repc(i, n) for (int i = 0; i <= int(n); i++)
+#define rrepc(i, n) for (int i = int(n); i >= 0; i--)
+#define repi(i, a, b) for (int i = int(a); i < int(b); i++)
+#define repic(i, a, b) for (int i = int(a); i <= int(b); i++)
 #define each(x, y) for (auto &x : y)
+#define all(a) (a).begin(), (a).end()
+#define bit(b) (1ll << (b))
 
 using namespace std;
 
 using i32 = int;
 using i64 = long long;
+using u64 = unsigned long long;
+using f80 = long double;
 using vi32 = vector<i32>;
 using vi64 = vector<i64>;
+using vu64 = vector<u64>;
+using vf80 = vector<f80>;
+using vstr = vector<string>;
+
+inline void yes() { cout << "Yes" << '\n'; exit(0); }
+inline void no() { cout << "No" << '\n'; exit(0); }
+inline i64 gcd(i64 a, i64 b) { if (min(a, b) == 0) return max(a, b); if (a % b == 0) return b; return gcd(b, a % b); }
+inline i64 lcm(i64 a, i64 b) { return a / gcd(a, b) * b; }
+inline u64 xorshift() { static u64 x = 88172645463325252ull; x = x ^ (x << 7); return x = x ^ (x >> 9); }
+void solve(); int main() { ios::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(16); solve(); return 0; }
+template <typename T> class pqasc : public priority_queue<T, vector<T>, greater<T>> {};
+template <typename T> class pqdesc : public priority_queue<T, vector<T>, less<T>> {};
+template <typename T> inline void amax(T &x, T y) { if (x < y) x = y; }
+template <typename T> inline void amin(T &x, T y) { if (x > y) x = y; }
+template <typename T> inline T power(T x, i64 n, T e = 1) { T r = e; while (n > 0) { if (n & 1) r *= x; x *= x; n >>= 1; } return r; }
+template <typename T> istream& operator>>(istream &is, vector<T> &v) { each(x, v) is >> x; return is; }
+template <typename T> ostream& operator<<(ostream &os, vector<T> &v) { rep(i, v.size()) { if (i) os << ' '; os << v[i]; } return os; }
+template <typename T, typename S> istream& operator>>(istream &is, pair<T, S> &p) { is >> p.first >> p.second; return is; }
+template <typename T, typename S> ostream& operator<<(ostream &os, pair<T, S> &p) { os << p.first << ' ' << p.second; return os; }
 
 template <typename T, typename S>
 struct LazySegmentTree {
@@ -39,7 +67,9 @@ struct LazySegmentTree {
   }
 
   void build() {
-    rrep(i, n) v[i] = f(v[i * 2 + 0], v[i * 2 + 1]);
+    for (int i = n - 1; i >= 0; i--) {
+      v[i] = f(v[i * 2 + 0], v[i * 2 + 1]);
+    }
   }
 
   void propagate(int k, int l, int r) {
@@ -71,6 +101,10 @@ struct LazySegmentTree {
       query(a, b, k * 2 + 0, l, (l + r) / 2),
       query(a, b, k * 2 + 1, (l + r) / 2, r)
     );
+  }
+
+  T operator[](size_t i) {
+    return query(i, i + 1);
   }
 };
 
@@ -115,9 +149,19 @@ struct TwoMonthScheduling {
         int md = (ok + ng) / 2;
         (check(i, md) ? ok : ng) = md;
       }
-      segs[i].update(i, ok + 1, segs[j].query(i, i + 1) + 1);
+      segs[i].update(i, ok + 1, segs[j][i] + 1);
     }
 
-    return segs[n].query(n, n + 1);
+    return segs[n][n];
   }
 };
+
+void solve() {
+  int workers;
+  cin >> workers;
+  int n, m;
+  cin >> n >> m;
+  vi32 a(n), b(m), c(n), d(m);
+  cin >> a >> b >> c >> d;
+  cout << TwoMonthScheduling().minimumMonths(workers, a, b, c, d) << endl;
+}
